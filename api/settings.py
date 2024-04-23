22b6cc48ba4fd1c46d34c14a8db375eadc33fbaa
Sebastian Hernadez <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2024-04-23 18:32:43 -0500

    bajar front-end

--- api/settings.py
+++ api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'cursos',
     'profesor'
 ]
@@ -132,3 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
--- profesor/urls.py
+++ profesor/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, re_path,include
 from . import views
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 
 router = routers.DefaultRouter()
 router.register(r'profesor', views.profesorViewSet, 'profesor.views')   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title="Profesor API"))
 ]
