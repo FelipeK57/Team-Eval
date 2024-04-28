3724cef8cf51aae8de46aa73c34c5938c8af48e7
JuanPalacios <juan.palacios.casanova@correounivalle.edu.co>
2024-04-28 11:26:52 -0500

    configurando el modelo rubrica

--- criterio_evaluacion/models.py
+++ criterio_evaluacion/models.py
@@ -5,3 +5,6 @@ class criterio_Evaluacion(models.Model):
     descripcion = models.CharField(max_length=500)
     peso = models.IntegerField(null=False)
     escala = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.descripcion
--- criterio_evaluacion/views.py
+++ criterio_evaluacion/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from serializers import criterio_EvaluacionSerializer
+from .models import criterio_Evaluacion
 
 # Create your views here.
+class criterio_EvaluacionView(viewsets.ModelViewSet):
+    serializer_class = criterio_EvaluacionSerializer
+    queryset = criterio_Evaluacion.objects.all()
