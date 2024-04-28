3e4ec5ed1f2823b704e20354467a487c3fc95eec
JuanPalacios <juan.palacios.casanova@correounivalle.edu.co>
2024-04-28 11:35:27 -0500

    configurando modelo evaluacion

---
+++ evaluacion/urls.py
@@ -0,0 +1,9 @@
+from django import path, include 
+from rest_framework import routers
+from evaluacion import views
+
+router = routers.DefaultRouter()
+router.register(r'evaluacion', views.evaluacionView, 'evaluacion')
+urlpatterns = [
+    path("api/v1/", include(router.urls)) 
+]
