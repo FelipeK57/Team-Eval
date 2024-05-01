b1fb204c6ddc0bbca192cf766c339491aab02b94
JuanPalacios <juan.palacios.casanova@correounivalle.edu.co>
2024-05-01 14:36:00 -0500

    relaciones en criterio, rubrica, evaluacion y curso

--- criterio_evaluacion/models.py
+++ criterio_evaluacion/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from rubrica.models import rubrica_Evaluacion
 
 # Create your models here.
 class criterio_Evaluacion(models.Model):
     descripcion = models.CharField(max_length=500)
     peso = models.IntegerField(null=False)
     escala = models.IntegerField(null=False)
+    rubricas = models.ManyToManyField(rubrica_Evaluacion)
 
     def __str__(self):
         return self.descripcion
--- grupo/models.py
+++ grupo/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from cursos.models import Cursos
 
 # Create your models here.
 class Grupo(models.Model):
     nombre = models.CharField(max_length=200)
     proyectoAsignado = models.CharField(max_length=200)
+    cursos = models.ManyToManyField(Cursos)
 
     def __str__(self):
         return self.nombre
