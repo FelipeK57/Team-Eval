1330ffcca20e2e422a7f082364fc6371716166ac
Camilo Gonzalez <juan.gonzalez.ortiz@correounivalle.com.co>
2024-05-01 22:31:56 -0500

    Creacion cambiar contraseña

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import LoginAdmin from "./pages/LoginAdmin/LoginAdmin.jsx";
 import ActualizacionCorreo from "./pages/ActualizacionCorreo/ActualizacionCorreo.jsx";
 import AvisoCorreo from "./pages/ActualizacionCorreo/AvisoCorreo.jsx";
 import AvisoContrasena from "./pages/CambiarContraseña/AvisoContraseña.jsx";
+import CambiarContrasena from "./pages/CambiarContraseña/CambiarContraseña.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -20,6 +21,7 @@ function App() {
         <Route path="/LoginAdmin" element={<LoginAdmin/>} />
         <Route path="/ActualizacionCorreo" element={<ActualizacionCorreo />} />
         <Route path="/AvisoCorreo" element={<AvisoCorreo />} />
+        <Route path="/CambiarContraseña" element={<CambiarContrasena />} />
         <Route path="/AvisoContraseña" element={<AvisoContrasena />} />
         <Route path="/" element={<Home />} />
         <Route path="/Student" element={<HomeStudent />} />
