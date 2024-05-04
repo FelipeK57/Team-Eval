2ae98197137ec00afb7c5d16f63fadb3ca4a580a
KevinFelipe <orozcokevin57bo@gmail.com>
2024-05-04 12:50:00 -0500

    actualizarCorreo-proceso

--- client/src/components/NavbarStudent.jsx
+++ client/src/components/NavbarStudent.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Utilities/NavbarStudent.css';
 import "../../public/Logo.png";
+import { Link } from 'react-router-dom';
 
 const NavbarStudent = () => {
   return (
     <nav className="navbar" >
       <div className='imagen'><img src="../../public/Logo.png" alt="logo" /></div>
       <h1 href="#" className="navbarh1"><b>¡Estudiante!</b></h1>
-      <a href="#" className="navbara"><b>Mi Cuenta</b></a>
-      <a href="#" className="navbarb"><b>Informes</b></a>
+      <Link to="/MiCuenta" className="navbara"><b>Mi Cuenta</b></Link>
+      <Link href="#" className="navbarb"><b>Informes</b></Link>
     </nav>
   );
 };
--- client/src/pages/ActualizacionCorreo/ActualizacionCorreo.jsx
+++ client/src/pages/ActualizacionCorreo/ActualizacionCorreo.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import Login2 from "../../components/Login2";
 
 function ActualizacionCorreo() {
-    return (
-        <Login2 Title="Actualizar correo"
-                Type1="mail" 
-                Field1="Nuevo Correo"
-                Type2="mail"
-                Field2="Confirmar Correo"
-                ForgotPassword={false}
-                Button="Hecho"
-                NavigateRoute=""/>
-    )
+  const [newEmail, setNewEmail] = useState("");
+  const [confEmail, setConfEmail] = useState("");
+
+  const handleNewChange = (e) => {
+    setNewEmail(e.target.value);
+  };
+
+  const handleConfChange = (e) => {
+    setConfEmail(e.target.value);
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/changeEmail/", {
+        codigo: codigo,
+        password: password,
+      });
+      console.log(newEmail + " " + confEmail);
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+
+  return (
+    <Login2
+      Title="Actualizar correo"
+      Type1="mail"
+      Field1="Nuevo Correo"
+      onChangeField1={handleNewChange}
+      onChangeField2={handleConfChange}
+      valueField1={newEmail}
+      valueField2={confEmail}
+      onClick={handleClick}
+      Type2="mail"
+      Field2="Confirmar Correo"
+      ForgotPassword={false}
+      Button="Hecho"
+      NavigateRoute="/login"
+    />
+  );
 }
 
 export default ActualizacionCorreo;
