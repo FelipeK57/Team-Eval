10058707dc0c5a964f0d6684ca520598784311dd
Camilo Gonzalez <juan.gonzalez.ortiz@correounivalle.com.co>
2024-05-11 23:08:26 -0500

    De nuevo se me olvido hacer una nueva rama :V

--- client/src/pages/CursosProfe/CursoP.jsx
+++ client/src/pages/CursosProfe/CursoP.jsx
@@ -1,6 +1,7 @@
 import "./CursoP.css";
 import NavbarProfesor from "../../components/NavbarProfesor";
 import Button2 from "../../components/Utilities/Button2";
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function CursosProfe() {
 
@@ -8,7 +9,11 @@ function CursosProfe() {
         <div className="CursosProfe">
             <NavbarProfesor />
             <div className="cursed"><h1>Editar Rubricas <br />Predeterminadas</h1></div>
-            <div className="cardex"></div>
+            <div className="cardex">
+                <button>
+                    <SettingsIcon />
+                </button>
+            </div>
             <div className="linea-vertical"></div>
             <div className="cursed2"><h1>Crear Rubrica</h1></div>
             <div className="cardex2"></div>
@@ -16,11 +21,11 @@ function CursosProfe() {
             <div className="corsel"><h1>Cursos de <b>Ejemplo</b></h1></div>
             <div className="cardex3">
             <div className="cardex32"><h1>Ejemplo <br />Completado</h1></div>  
-            <div class="line-horizonte"></div>
+            <div className="line-horizonte"></div>
             </div>
             <div className="cardex4">
             <div className="cardex42"><h1>Ejemplo por <br />Completar</h1></div>  
-            <div class="line-horizonte"></div>
+            <div className="line-horizonte"></div>
             </div>
         </div>
     )
