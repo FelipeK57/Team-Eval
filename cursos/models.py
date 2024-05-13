e227dfcd1df87b89e2dcb981aab7493c99235c00
JuanPalacios <juan.palacios.casanova@correounivalle.edu.co>
2024-05-13 12:08:19 -0500

    guardando cambios

--- cursos/models.py
+++ cursos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from profesor.models import Profesor
 from grupo.models import Grupo
+from estudiantes.models import Estudiante
+from evaluacion.models import evaluacion
 
 # Create your models here.
 class Cursos(models.Model):
@@ -9,7 +11,8 @@ class Cursos(models.Model):
     estado = models.BooleanField(default=True, null=False)
     periodoacademico = models.CharField(max_length=7, default=2024-1)
     profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, default=1)
-    grupos = models.ManyToManyField(Grupo)
+    estudiantes = models.ManyToManyField(Estudiante) 
+    evaluaciones = models.ManyToManyField(evaluacion)
 
     def __str__(self):
         return self.nombre
--- evaluacion/models.py
+++ evaluacion/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from rubrica.models import rubrica_Evaluacion
 from grupo.models import Grupo
+from informes.models import informes
+from grupo.models import Grupo
 import datetime
 
 
@@ -9,7 +11,8 @@ class evaluacion(models.Model):
     fecha = models.DateField (null=False)
     rubrica = models.OneToOneField(rubrica_Evaluacion,on_delete=models.CASCADE, null=False)
     grupo = models.OneToOneField(Grupo,on_delete=models.CASCADE ,null=False)
-
+    informe = models.ManyToManyField(informes)
+    grupo = models.ManyToManyField(Grupo)
 
 
     
