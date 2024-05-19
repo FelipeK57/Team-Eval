61a75636aa5b7c2dda1cc4ee037a3e6adcbb3c92
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-05-18 23:40:18 -0500

    Interface importar cursos

--- client/src/pages/Home/ImportarCursos.jsx
+++ client/src/pages/Home/ImportarCursos.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import "./ImportarCursos.css";
 import UploadFileIcon from '@mui/icons-material/UploadFile';
+import NoQuieroCrearMasNavbars from "../../components/NoQuieroCrearMasNavbars";
 
 function ImportarCursos() {
   const [file, setFile] = useState(null);
@@ -27,9 +28,34 @@ function ImportarCursos() {
   };
 
   return (
-    <div className="ImportarCursos">
-      <input  className="upload" onChange={handleFileChange} type="file" /> 
-      <button onClick={sendFile}>Enviar</button>
+    <div className="ImportarCursosContainer">
+      <div className="ContainerImportarCursos">
+        <div className="ImportarCursos">
+          <NoQuieroCrearMasNavbars />
+          <input className="upload" type="file" onChange={handleFileChange} />
+          <div className="FileContainer">
+            <h1 className="ButtonText">Subir Archivo</h1>
+            <button onClick={() => document.querySelector('.upload').click()}>
+              <UploadFileIcon sx={{ fontSize: 50 }} />
+            </button>
+          </div>
+          <label className="fileName" htmlFor="file">{file?.name}</label>
+
+        </div>
+        <div className="Recommendations">
+          <h1>Recomendaciones</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ratione porro ut sapiente dicta earum eos culpa mollitia cumque quidem velit id facilis ea fugit quod unde debitis, reprehenderit neque.</p>
+        </div>
+
+        <div className="EnviarContainer">
+          <button onClick={sendFile}>Enviar</button>
+        </div>
+      </div>
+      <div className="ContainerImportarCursos2">
+        <div className="Recommendations Recommendations2">
+          <h1> imagenes de Recomendaciones</h1>
+        </div>
+      </div>
     </div>
   );
 }
