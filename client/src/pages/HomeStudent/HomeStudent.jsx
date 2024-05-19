6f153ff220211a9e4519d1206330a6a44b61e387
Sebastian Hernadez <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2024-05-19 12:34:44 -0500

    cambio necesario

--- client/src/pages/HomeStudent/HomeStudent.jsx
+++ client/src/pages/HomeStudent/HomeStudent.jsx
@@ -2,8 +2,29 @@ import "./HomeStudent.css";
 import NavbarStudent from "../../components/NavbarStudent";
 import Button2 from "../../components/Utilities/Button2";
 import { Link } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
 
 function HomeStudent() {
+  const navigate = useNavigate();
+
+
+    useEffect(() => {
+      const verificarSesion = () => {
+        const loggedIn = Cookies.get("loggedIn");
+        const userId = Cookies.get("codigo");
+  
+        if (loggedIn === "true" && userId) {
+          console.log("El usuario ha iniciado sesión. ID de usuario:", userId);
+        } else {
+          console.log("El usuario no ha iniciado sesión.");
+          navigate("/Login");
+        }
+      };
+  
+      verificarSesion();
+    }, [navigate]);
   return (
     <div className="Home2">
       <NavbarStudent />
