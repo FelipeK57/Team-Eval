a9ab0efd8224318dc5ef0905d8e0d5adc983a494
JuanDi <juan.leon.reyes@correounivalle.edu.co>
2024-05-21 20:05:05 -0500

    Lista de estudiantes

--- client/src/pages/CursosProfe/EstudiantesLista.jsx
+++ client/src/pages/CursosProfe/EstudiantesLista.jsx
@@ -9,7 +9,6 @@ import { useNavigate } from "react-router-dom";
 function Estudiantes() {
     const [estudiantes, setEstudiantes] = useState([]);
     //const navigate = useNavigate();
-  
    // const AgregarEstudiantes = () => {
    //   navigate("/AgregarE");
    // };
