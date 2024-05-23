8ffe4d1d5de66a92ab7c3821bd41712b0365d033
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-05-23 16:52:08 -0500

    commit 2

--- client/src/components/CardForm.jsx
+++ client/src/components/CardForm.jsx
@@ -99,7 +99,7 @@ function CardForm(props) {
                             </div> : null}
                             <Button LineaBoton={false} Boton="Registrar" color="rgb(15, 65, 117)" fontColor="white" onClick={props.onClick} />
                         </form>
-                        <button className="AgregarEstudiantes">Agregar estudiantes</button>
+                        {props.Title === "Agregar Curso" ? <button className="AgregarEstudiantes">Agregar estudiantes</button> : null}
                     </div>
                 </div>
             </div>
