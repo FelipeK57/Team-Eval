fcaea1510b37dc54d1ce14599db8828dbe5b262b
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-06-02 21:19:01 -0500

    siempre se me olvida crear otra rama pana

---
+++ client/src/pages/RubricasProfe/RubricasProfe.jsx
@@ -0,0 +1,34 @@
+import NavbarProfesor from "../../components/NavbarProfesor";
+import LargeButton from "../../components/Utilities/LargeButton";
+import AddIcon from '@mui/icons-material/Add';
+import Edit from '@mui/icons-material/Edit';
+import { useNavigate } from 'react-router-dom';
+
+function RubricasProfe() {
+
+    const navigate = useNavigate();
+
+    const Rubrica = () => {
+        navigate("/TablaRubricas");
+    }
+
+    return (
+        <div className="RubricasContainer">
+            <NavbarProfesor/>
+            <div className="TitleRubricas">
+                <h1>Editar Rubricas </h1>
+            </div>
+            <div className="OpcionesRubricas">
+                <div className="NombreRubricas">
+                    <h1>Rubrica predeterminada</h1>
+                </div>
+                <div className="OpcionRubricas">
+                    <LargeButton icon={<Edit />} text="Criterio numero 1" OnClick={Rubrica} />
+                    <LargeButton icon={<AddIcon/>} text={false} OnClick={Rubrica} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RubricasProfe;
