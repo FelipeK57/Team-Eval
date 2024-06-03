a9a077843d98a75c8bd901af9b9712fdd49ea563
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-06-02 19:57:32 -0500

    barra de busqueda desplegable de profesores

--- client/src/pages/Admin/Profesores.jsx
+++ client/src/pages/Admin/Profesores.jsx
@@ -7,18 +7,21 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie'
 import PopUp from "../../components/Utilities/PopUp";
+import Search from "@mui/icons-material/Search";
+import Field from "../../components/Utilities/Field";
 
 function Profesores() {
   const [profesores, setProfesores] = useState([]);
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const [advice, setAdvice] = useState("");
+  const [searchProfesores, setSearchProfesores] = useState(false);
 
   const AgregarProfesores = () => {
     navigate("/AgregarP");
   };
 
-  const ProfesoresDeshabilitados = () =>{
+  const ProfesoresDeshabilitados = () => {
     navigate("/ProfeDes")
   }
   useEffect(() => {
@@ -35,33 +38,41 @@ function Profesores() {
     fetchStudentCourses();
   }, []);
 
-  const EditarProfesor = (identificacion, nombre, email ) => {
+  const EditarProfesor = (identificacion, nombre, email) => {
     Cookies.set('profesorIdentificacion', identificacion, { expires: 1 });
     Cookies.set('profesorNombre', nombre, { expires: 1 });
     Cookies.set('profesorEmail', email, { expires: 1 });
     navigate("/ProfeEditar");
   };
 
-  const deshabilitarProfesor =  async ( identificacion) => {
+  const deshabilitarProfesor = async (identificacion) => {
     try {
-        await axios.post("http://localhost:8000/editEstado_profesor/", {
-          identificacion: identificacion,
-          estado: false,
-        });
-        setAdvice("Profesor deshabilitado con exito");  
-        setOpen(true);   
-         
+      await axios.post("http://localhost:8000/editEstado_profesor/", {
+        identificacion: identificacion,
+        estado: false,
+      });
+      setAdvice("Profesor deshabilitado con exito");
+      setOpen(true);
+
     } catch (error) {
       console.error(error);
-    } 
-    
+    }
+
   }
 
   const popup = (e) => {
     e.preventDefault();
     setOpen(!open);
     window.location.reload();
-};
+  };
+
+  const BuscarButton = () => {
+    setSearchProfesores(true);
+    const searchField = document.querySelector(".SearchFieldProfesores input");
+    setTimeout(() => {
+      searchField.focus();
+    }, 0);
+  };
 
   return (
     <div className="ContainerProfesores">
@@ -70,8 +81,21 @@ function Profesores() {
       </div>
       <div className="TitleProfesores">
         <h1>Profesores</h1>
+        <div className="SearchProfesores">
+          <button className="SearchButtonProfesores" onClick={BuscarButton}>
+            <Search sx={{ fontSize: 30, color: "white" }} />
+          </button>
+          <div className={searchProfesores === true ? "SearchFieldProfesores Active" : "SearchFieldProfesores Inactive"}
+            onBlur={() => setSearchProfesores(false)}>
+            <Field
+              LineaBoton={false}
+              Boton=""
+              color="rgb(15, 65, 118)"
+              fontColor="white"
+            />
+          </div>
+        </div>
       </div>
-      <div className="Search"></div>
       <div className="AgregarListProfesores">
         <Button
           LineaBoton={false}
@@ -83,11 +107,11 @@ function Profesores() {
       </div>
       <div className="CursosDes">
         <Button
-        LineaBoton={false}
-        Boton="Profesores desabilitados"
-        color="rgb(15,65,118)"
-        fontColor="white"
-        onClick={ProfesoresDeshabilitados}
+          LineaBoton={false}
+          Boton="Profesores desabilitados"
+          color="rgb(15,65,118)"
+          fontColor="white"
+          onClick={ProfesoresDeshabilitados}
         />
       </div>
       <div className="ListaProfesores">
@@ -96,7 +120,7 @@ function Profesores() {
             <ListItems
               Nombre1={profesor.user.first_name}
               Codigo1={profesor.identificacion}
-              onClickEdit={() => EditarProfesor(profesor.identificacion, profesor.user.first_name, profesor.user.email)} 
+              onClickEdit={() => EditarProfesor(profesor.identificacion, profesor.user.first_name, profesor.user.email)}
               onClickDelete={() => deshabilitarProfesor(profesor.identificacion)}
               Buttons={true}
             />
@@ -104,13 +128,13 @@ function Profesores() {
         ))}
       </div>
       <PopUp open={open}
-                SetOpen={setOpen}
-                Advice={advice}
-                Width={"100%"}
-                Button1="volver"
-                onClick1={popup}
-                
-            />
+        SetOpen={setOpen}
+        Advice={advice}
+        Width={"100%"}
+        Button1="volver"
+        onClick1={popup}
+
+      />
     </div>
   );
 }
